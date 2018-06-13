e101b64667c8b596641c01013ba1256e3bcdc606
ZeRl0l <38573410+ZeRl0l@users.noreply.github.com>
2018-06-13 11:10:03 -0300

    Update pgm.h

--- pgm.h
+++ pgm.h
@@ -1,15 +1,158 @@
-typedef struct image {
-    int larg;
-    int alt;
-    int maximo;
-    int *data;
-}img;
+typedef struct pgm {
+	int altura;
+	int largura;
+	int maximo;
+	int **data;
 
-FILE* fp;
-img* pPgm;
-int ler = 0;
-char nome_arquivo [265];
-char lerarq [256];
-int palavra;
+}pgm;
 
-void escreverigm (struct image* img);
+pgm* lerPGM(const char *nome_arquivo, pgm* pPgm){
+	
+	FILE *fp;
+	char ler[256];
+	int i, j;
+	int lo, hi;
+	
+	printf("Qual o nome do arquivo a ser lido: ");
+	scanf("%s", nome_arquivo);
+	
+	fp = fopen(nome_arquivo,"r");
+	
+	if(fp == NULL){
+		printf("Nao foi possivel abrir o arquivo.\n");
+		exit(1);
+	}
+	
+	printf("Lendo o arquivo %s...\n\n", nome_arquivo);
+	
+	fscanf(fp,"%s",ler);
+	
+	if(strcmp(ler, "P2") == 0){
+		
+		printf("TIPO VALIDO!\n\n");
+		fscanf(fp,"%s", ler);
+	}
+	
+	split(fp);
+	fscanf(fp, "%d", &pPgm -> altura);
+	split(fp);
+	fscanf(fp, "%d", &pPgm -> largura);
+	slipt(fp);
+	fscanf(fp, "%d", &pPgm -> maximo);
+	fgetc(fp);
+	
+	pPgm -> data = alocar_memoria(pPgm -> altura, pPgm -> largura);
+	
+	if(pPgm -> maximo > 255){
+		for(i = 0; i < pPgm -> altura; ++i) {
+			for(j = 0; j < pPgm -> largura; ++j){
+				hi = fgetc(fp);
+				lo = fgetc (fp);
+				pPgm -> data[i][j] = (hi << 8) + lo;
+			}	
+		}
+	}
+	else {
+		for(i = 0; i < pPgm -> altura; ++i){
+			for(j = 0; i < pPgm -> largura; ++j){
+				lo = fgetc(fp);
+				pPgm -> data[i][j] = lo;
+			}
+		}
+		
+		fclose(fp);
+		return pPgm;
+}
+
+void escreverPGM(const char *novo_arquivo, const pgm* pPgm){
+	FILE *nfp;
+	int i, j;
+	int hi, lo;
+	
+	printf("--------ESCREVER NOVO ARQUIVO--------\n\n");
+	
+	printf("O arquivo deve ser escrito no formato ./pasta/nomedoarquivo.ascii.pgm\n\n");
+	
+	printf("Digite o nome do novo arquivo: ");
+	scanf("%s", novo_arquivo);
+	
+	nfp = fopen(novo_arquivo, "w");
+	
+	if(nfp == NULL){
+		printf("Nao foi possivel abrir o arquivo.\n");
+		exit(1);	
+	}
+	
+	fprintf(nfp,"P2");
+	fprintf(nfp,"%d %d", pPgm -> largura, pPgm -> altura);
+	fprintf(nfp, "%d", pPgm -> maximo);
+	
+	/*if(pPgm -> maximo > 255){
+	for(i = 0; i < pPgm -> altura; ++i) {
+		for(j = 0; j < pPgm -> largura; ++j){
+			hi = fgetc(fp);
+			lo = fgetc (fp);
+			pPgm -> data[i][j] = (hi << 8) + lo;
+			}	
+		}
+	}
+	else {
+		for(i = 0; i < pPgm -> altura; ++i){
+			for(j = 0; i < pPgm -> largura; ++j){
+				lo = fgetc(fp);
+				pPgm -> data[i][j] = lo;
+		}
+	}*/
+	fclose(nfp);
+	desalocar_memoria(pPgm -> data, pPgm -> altura);
+
+}
+
+void split(FILE *fp0){
+	
+	int ch;
+	char linhas[100];
+	
+	while((ch = fgetc(fp0)) != EOF && isspace(ch));
+	
+	if(ch == '#'){
+		fgets(linhas, sizeof(linhas), fp0);
+		split(fp0);
+	} else {
+		fseek(fp, -1, SEEK_CUR);
+	}
+
+}
+
+void **alocar_memoria(struct pmg* pgm){
+	
+	int **val;
+	int i;
+	
+	val = (int **)malloc(sizeof(int *)* largura);
+	if (val == NULL){
+		printf("Nao foi possivel alocar memoria.");
+		return NULL;
+	}
+	
+	for(i = 0; i < altura; i++){
+		val[i] = (int *)malloc(sizeof(int) * altura);
+			printf("Nao foi possivel alocar memoria");
+			return NULL;
+	}
+	
+	return val;
+
+}
+
+void desalocar_memoria(int **data, int altura){
+
+	int i;
+	
+	for (i = 0; i < altura; ++i){
+		free(data[i]);
+	
+	}
+	
+	free (data);
+}
