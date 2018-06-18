ca57e4c05b0671dd74d6d240dfe441906b6c84a8
ZeRl0l <38573410+ZeRl0l@users.noreply.github.com>
2018-06-18 13:56:55 -0300

    Update mask.h

--- mask.h
+++ mask.h
@@ -20,9 +20,7 @@ int** aloca_memo(int largura, int altura){
 	return val;
 }
 
-int convolucao (int **data, int laplace[3][3]){
-	
-	int i, j;
+int convolucao (int i, int j ,int laplace[][3],int**data){
 	
 	int p = 0, q = 0, temp;
 	int element = 0;
@@ -44,23 +42,47 @@ int convolucao (int **data, int laplace[3][3]){
 	return element;
 }
 
-int** testando(int **data, int largura, int altura, int op1){
+int** filtros(int **data2, int linha , int coluna, int op1){
 	int matriz[3][3];
 	
-	int i;
-	int j;
+	
 	
 	if(op1 == 1){
-    	for(i = 0; i < 3; i++){
-			for(j = 0; j <3; j++){
+    	for(int i = 0; i < 3; i++){
+			for(int j = 0; j <3; j++){
 				matriz[i][j] = laplace[i][j];
 			}
 		}	
  	}else if(op1 == 2){
-		for(i = 0; i < 3; i++){
-			for(j = 0; j < 3; j++){
+		for(int i = 0; i < 3; i++){
+			for(int j = 0; j < 3; j++){
 				matriz[i][j] = sobel[i][j];
 			}
 		}
 	}
+
+	int **val = aloca_memo(coluna+2,linha+2);
+		
+		for (int i=0;i<linha;i++){
+			for(int j=0;j<coluna;j++){
+				val[i][j]=0;
+			}
+		}
+		for (int i=1;i<linha+1;i++){
+			for(int j=1;j<coluna+1;j++){
+				val[i][j]= data2[i-1][j-1];
+			}
+		}
+	
+		for (int i=1;i<linha-1;i++){
+			for(int j=1;j<coluna-1;j++){
+				val[i][j]= convolucao(i,j,matriz,data2);
+			}
+		}
+		for (int i=1;i<linha+1;i++){
+			for(int j=1;j<coluna+1;j++){
+				data2[i-1][j-1]= val[i][j];
+			}
+		}
+	return val;
 }
